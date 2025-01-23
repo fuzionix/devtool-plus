e69485f19dffdce0723df3845a28b6614e9af728
Taylon Chan <tchan199616@gmail.com>
2025-01-23 23:14:58 +0800

    💄 | Update Tailwind base styles

--- src/tools/components/base64-encoder/Base64Encoder.ts
+++ src/tools/components/base64-encoder/Base64Encoder.ts
@@ -9,7 +9,8 @@ export class Base64Encoder extends BaseTool {
     protected setupTemplate(): void {
         this.toolContainer.innerHTML = `
             <div class="tool-container">
-                <p class="opacity-75">Tool Base</p>
+                <p class="opacity-75">Encode text to Base64 or decode Base64 to text</p>
+                <input id="input" type="text" placeholder="Enter text to encode" />
             </div>
         `;
     }
