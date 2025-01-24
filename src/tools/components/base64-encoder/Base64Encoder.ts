7ced16d259822e5327c05c2ce6ddf870d7752f0f
Taylon Chan <tchan199616@gmail.com>
2025-01-24 23:20:13 +0800

    💄 | Add button component

--- src/tools/components/base64-encoder/Base64Encoder.ts
+++ src/tools/components/base64-encoder/Base64Encoder.ts
@@ -10,7 +10,10 @@ export class Base64Encoder extends BaseTool {
         this.toolContainer.innerHTML = `
             <div class="tool-container">
                 <p class="opacity-75">Encode text to Base64 or decode Base64 to text</p>
+                <hr />
                 <input id="input" type="text" placeholder="Enter text to encode" />
+                <button id="encode" class="btn-primary mt-2">Encode</button>
+                <textarea id="output" class="mt-2" placeholder="Output will appear here"></textarea>
             </div>
         `;
     }
