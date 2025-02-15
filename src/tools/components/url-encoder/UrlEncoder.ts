48982d63061bcb45262e21f401d781158493b956
Taylon Chan <tchan199616@gmail.com>
2025-02-15 19:38:38 +0800

    💄 | Update radio button spacing

--- src/tools/components/url-encoder/UrlEncoder.ts
+++ src/tools/components/url-encoder/UrlEncoder.ts
@@ -33,7 +33,7 @@ export class UrlEncoder extends BaseTool {
                             class="radio-group-button flex justify-center items-center"
                             @click=${() => this.handleModeChange('encode')}
                         >
-                            <span class="text-xs opacity-75 mr-1">
+                            <span class="text-xs opacity-75 mr-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevrons-left-right-square"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="m10 15-3-3 3-3"/><path d="m14 9 3 3-3 3"/></svg>
                             </span>
                             <h4>Encode</h4>
@@ -44,7 +44,7 @@ export class UrlEncoder extends BaseTool {
                             class="radio-group-button flex justify-center items-center"
                             @click=${() => this.handleModeChange('decode')}
                         >
-                            <span class="text-xs opacity-75 mr-1">
+                            <span class="text-xs opacity-75 mr-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-code"><polyline points="16 18 22 12 16 6"/><polyline points="8 6 2 12 8 18"/></svg>
                             </span>
                             <h4>Decode</h4>
