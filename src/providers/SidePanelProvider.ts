3147d5f2460921efbcd6667c23fec82dbe73ed3c
Taylon Chan <tchan199616@gmail.com>
2025-08-10 17:22:24 +0800

    ✨ | Implement content security policy in editors

--- src/providers/SidePanelProvider.ts
+++ src/providers/SidePanelProvider.ts
@@ -90,7 +90,7 @@ export class SidePanelProvider implements vscode.WebviewViewProvider {
                 <div id="empty-state" class="pt-2 px-4">
                     <div class="search-container mb-2">
                         <div class="relative">
-                            <input type="text" id="tool-search" placeholder="E.g. UUID Generator" class="w-full py-1 px-2" autofocus />
+                            <input type="text" id="tool-search" placeholder="E.g. UUID Generator" class="w-full py-1 px-2" />
                             <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-2 top-1/2 transform -translate-y-1/2" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
                         </div>
                     </div>
