49f5032ea8e52e9824c7530bcc4811f54d23313a
Taylon Chan <tchan199616@gmail.com>
2025-08-25 21:31:22 +0800

    💄 | Extend Tailwind CSS theme with custom color schema

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -5,7 +5,45 @@ module.exports = {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    default: "#22a5f1",
+                    100: "#c6e0fb",
+                    200: "#a8d1f9",
+                    300: "#87c2f6",
+                    400: "#61b3f4",
+                    500: "#22a5f1",
+                    600: "#2687c4",
+                    700: "#266b9a",
+                    800: "#225071",
+                    900: "#1c364b"
+                },
+                accent: {
+                    red: {
+                        default: "#de474a",
+                    },
+                    yellow: {
+                        default: "#f8c358",
+                    },
+                    green: {
+                        default: "#6cd29a",
+                    },
+                },
+                background: {
+                    default: "#fafafa",
+                    card: "#f6f6f6"
+                },
+                border: {
+                    default: "#d1d1d1",
+                    grid: "#dcdcdc",
+                },
+                text: {
+                    default: "#000000",
+                    secondary: "#8a8a8a"
+                }
+            }
+        },
     },
     plugins: [],
 }
