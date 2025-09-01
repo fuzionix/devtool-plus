4f63da050cf93e41b6af812f4a2217c6f75f0d30
FUNGCHAN\qwert <tchan199616@gmail.com>
2025-09-02 03:18:49 +0800

    ✨ | Update website configuration

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig(({ mode }) => {
+    return {
+        base: '/devtool-plus/',
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, './src'),
+            }
+        },
+        build: {
+            outDir: 'dist',
+            minify: 'terser',
+            sourcemap: mode === 'development',
+            terserOptions: {
+                compress: {
+                    drop_console: true,
+                    drop_debugger: true,
+                },
+            },
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        react: ['react', 'react-dom'],
+                    }
+                }
+            }
+        },
+        server: {
+            open: true,
+            cors: true
+        },
+        css: {
+            devSourcemap: true,
+        },
+        plugins: [react()],
+    }
 })
